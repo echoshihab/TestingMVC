d5ee656aa6df15d7d5b92119bfa9bd19fe5b99f4
shihab <echoshihab@gmail.com>
2020-08-06 21:50:12 -0400

    initiate testing project

---
+++ TestingAspNetCoreMVC.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestingAspNetCoreMVC.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
