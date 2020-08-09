2ce6d9c16d999ceee378ec0895016901de1b3bf4
shihab <echoshihab@gmail.com>
2020-08-09 16:33:51 -0400

    converts tests to use mock repository

--- TestingAspNetCoreMVC.Tests/ControllerTests.cs
+++ TestingAspNetCoreMVC.Tests/ControllerTests.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using TestingAspNetCoreMVC.Web.Controllers;
 using TestingAspNetCoreMVC.Web.Data.Repository;
+using TestingAspNetCoreMVC.Web.Data.Repository.IRepository;
 using TestingAspNetCoreMVC.Web.Models;
 using Xunit;
 
@@ -15,8 +17,8 @@ namespace TestingAspNetCoreMVC.Tests
         public void IndexViewTypeEqualsViewResult()
         {
             //Arrange
-            var repository = new InMemoryUserRepository();
-            var controller = new HomeController(repository);
+            var repository = new Mock<IUserRepository>();
+            var controller = new HomeController(repository.Object);
             //Act
             var result = controller.Index();
 
@@ -27,8 +29,11 @@ namespace TestingAspNetCoreMVC.Tests
         [Fact]
         public void VerifyCountOfUsers()
         {
-            var repository = new InMemoryUserRepository();
-            var controller = new HomeController(repository);
+            var repository = new Mock<IUserRepository>();
+            repository.Setup(x => x.ListUsers()).Returns(new List<User>{
+                new User(), new User()
+            });
+            var controller = new HomeController(repository.Object);
             var result = Assert.IsType<ViewResult>(controller.List());
             var model = Assert.IsType<List<User>>(result.Model);
             Assert.Equal(2, model.Count());
